85c4c44aa97d2c1c1805eb16a46af779f3244cf3
Alper Konuralp <alper@konuralp.gen.tr>
2019-08-04 17:57:13 +0300

    I developed this application with two different methods.

--- src/main/java/com/dteknoloji/games/App.java
+++ src/main/java/com/dteknoloji/games/App.java
@@ -1,6 +1,7 @@
 package com.dteknoloji.games;
 
 import java.io.PrintStream;
+import java.time.Duration;
 import java.time.Instant;
 import java.util.HashSet;
 import java.util.Scanner;
@@ -17,15 +18,24 @@ public class App {
         System.out.print("End Number = ");
         int endNumber = scanner.nextInt();
 
-        Instant start = Instant.now();
-        findPrimeNumber(endNumber, System.out);
+        Instant starts = Instant.now();
+        int count = findPrimeNumber(endNumber, System.out);
+        Instant ends = Instant.now();
         System.out.println();
-        findPrimeNumber2(endNumber, System.out);
+        System.out.printf("Found %d items at %d milli-seconds.\n" , count, Duration.between(starts, ends).toMillis());
+        System.out.println();
+
+        starts = Instant.now();
+        count = findPrimeNumber2(endNumber, System.out);
+        ends = Instant.now();
+        System.out.println();
+        System.out.printf("Found %d items at %d milli-seconds.\n" , count, Duration.between(starts, ends).toMillis());
     }
 
-    private static void findPrimeNumber(int endNumber, PrintStream out) {
+    private static int findPrimeNumber(int endNumber, PrintStream out) {
         out.print("2");
         boolean found;
+        int count = 1;
         for (int index = 3; index <= endNumber; index += 2) {
             found = false;
             for (int subIndex = index - 1; subIndex >= 2; subIndex--) {
@@ -35,21 +45,24 @@ public class App {
                 }
             }
             if (!found) {
+                count++;
                 out.print(", ");
+                if (count % 20 == 0) out.println();
                 out.print(index);
             }
         }
+        return count;
     }
 
 
-    private static void findPrimeNumber2(int endNumber, PrintStream out) {
+    private static int findPrimeNumber2(int endNumber, PrintStream out) {
         Set<Integer> primeNumbers = new HashSet<Integer>();
         primeNumbers.add(2);
         out.print("2");
         boolean found;
         for (int index = 3; index <= endNumber; index += 2) {
             found = false;
-            for(int subIndex : primeNumbers){
+            for (int subIndex : primeNumbers) {
                 if (index % subIndex == 0) {
                     found = true;
                     break;
@@ -58,8 +71,10 @@ public class App {
             if (!found) {
                 primeNumbers.add(index);
                 out.print(", ");
+                if (primeNumbers.size() % 20 == 0) out.println();
                 out.print(index);
             }
         }
+        return primeNumbers.size();
     }
 }
